7614da435c38be7317db751a27c734f4ac350b86
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-02 11:10:23 +0900

    記事を操作するメソッドを持つrepositoryを作成

---
+++ src/main/java/jp/co/sample/repository/ArticleRepository.java
@@ -0,0 +1,28 @@
+package jp.co.sample.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import jp.co.sample.domain.Article;
+
+/**
+ * 記事を操作するメソッドをもつrepository.
+ * 
+ * @author junpei.oyama
+ *
+ */
+public class ArticleRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+	
+}
