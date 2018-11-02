79230247bf485787fa43a0a16fa970ae410f385c
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-02 11:49:28 +0900

    初期画面へフォワードするメソッドを作成

--- src/main/java/jp/co/sample/controller/ArticleController.java
+++ src/main/java/jp/co/sample/controller/ArticleController.java
@@ -1,9 +1,17 @@
 package jp.co.sample.controller;
 
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Article;
 import jp.co.sample.form.ArticleForm;
+import jp.co.sample.repository.ArticleRepository;
 
 /**
  * 記事情報をフォワードするcontroller.
@@ -15,11 +23,21 @@ import jp.co.sample.form.ArticleForm;
 @RequestMapping("/article")
 public class ArticleController {
 
-	private ArticleForm articleForm;
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	
+	/**
+	 * 初期画面を表示するメソッド.
+	 * 
+	 * @param model モデル
+	 * @return 入力画面
+	 */
 	@RequestMapping("/index")
-	public String index() {
-		
+	public String index(Model model) {
+		List<Article> articleList = articleRepository.findAll();
+		model.addAttribute("articleList", articleList);
+		return "index";
 	}
 	
 }
